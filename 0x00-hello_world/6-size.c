307c679aa3e65eb7feeed48c6f21314c21ae13c7
Samuel Tamunoateli <samueltamunoateli@gmail.com>
2023-06-18 16:15:47 -0700

    betty correction on printing sizes of data types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,16 +3,16 @@
 /**
  * main - Entry point
  *
- * Description - Using sizeof in printf to print the size of the various data types
+ * Description - Using sizeof to print the byte sizes of data types
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-printf("Size of a char: %lu byte(s)\n", sizeof(char));
-printf("Size of an int: %lu byte(s)\n", sizeof(int));
-printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
-printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
-printf("Size of a float: %lu byte(s)\n", sizeof(float));
-return (0);
+	printf("Size of a char: %lu byte(s)\n", sizeof(char));
+	printf("Size of an int: %lu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %lu byte(s)\n", sizeof(float));
+	return (0);
 }
