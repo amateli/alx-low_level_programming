0b19ecbc94b08f86c773563d17e8c7f71bcb922c
Samuel Tamunoateli <81063200+amateli@users.noreply.github.com>
2023-07-24 11:30:25 +0100

    code optimized 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,27 +9,18 @@
 
 void jack_bauer(void)
 {
-	int h1, h2, m1, m2;
+	int hour, mins;
 
-	for (h1 = 0; h1 <= 2; h1++)
+	for (hour = 0; hour < 24; hour++)
 	{
-		for (h2 = 0; h2 <= 9; h2++)
+		for (mins = 0; mins < 60; mins++)
 		{
-			if ((h1 <= 1 && h2 <= 9) || (h1 <= 2 && h2 <= 3))
-			{
-				for (m1 = 0; m1 <= 5; m1++)
-				{
-					for (m2 = 0; m2 <= 9; m2++)
-					{
-						_putchar(h1 + '0');
-						_putchar(h2 + '0');
-						_putchar(':');
-						_putchar(m1 + '0');
-						_putchar(m2 + '0');
-						_putchar('\n');
-					}
-				}
-			}
+			_putchar(hour/10);
+			_putchar(hour%10);
+			_putchar(':');
+			_putchar(mins/10);
+			_putchar(mins%10);
+			_putchar('\n');
 		}
 	}
 }
